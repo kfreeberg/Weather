625b939dbc3530ab19abb6850c5963f65a09e2b3
Kathleen Freeberg <kfreeberg@tse.local>
2019-07-29 11:07:38 -0400

    initial

---
+++ static/header.js
@@ -0,0 +1,29 @@
+document.write('  <div class="container-fluid"> \
+<nav class="navbar navbar-default">\
+        <div class="container">\
+            <div class="navbar-header">\
+                <a class="navbar-brand box" href="../index.html"> \
+                <span >Latitude</span></a>\
+            </div>\
+            <div class ="nav navbar-nav navbar-right">\
+                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown">Plots<span class="caret"></span></a>\
+                    <ul class="dropdown-menu">\
+                        <li><a href="MaxTemp.html">Temperature</a></li>\
+                        <li><a href="Humidity.html">Humidity</a></li>\
+                        <li><a href="Cloudiness.html">Cloudiness</a></li>\
+                        <li><a href="WindSpeed.html">Wind Speed</a></li></ul>\
+                </li> \
+                <li><a href="Plots.html"> Comparison</a></li>\
+                <li><a href="Data.html"> Data</a></li>\
+                <li><a href="../index.html"> Home </a></li> \
+            </div>\
+        </div>\
+    </nav>\
+</div>\
+\
+<ul class="nav nav-tabs">\
+<li class="active"><a href="MaxTemp.html">Temperature</a></li> \
+<li><a href="Humidity.html">Humidity</a></li> \
+<li><a href="Cloudiness.html">Cloudiness</a></li> \
+<li><a href="WindSpeed.html">Wind Speed</a></li> \
+</ul>');
